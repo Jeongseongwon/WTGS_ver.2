dc3b1fe6645996aa6f9acb19dffab653074c0ee3
Jeongseongwon <j001656100@gmail.com>
2023-01-16 18:42:58 +0900

    Revert "0116"

--- Assets/2.Script/3.Scene_script/Scene_1_2_controller.cs
+++ Assets/2.Script/3.Scene_script/Scene_1_2_controller.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using HighlightPlus;
 
 public class Scene_1_2_controller : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class Scene_1_2_controller : MonoBehaviour
     public GameObject Scriptbox;
     public GameObject Top_navigation;
     public GameObject Wind_particle;
-    public GameObject Study_title;
+    public GameObject Study_title_Intro_2;
    
     private Text text;
     bool toggle = true;
@@ -22,38 +23,48 @@ public class Scene_1_2_controller : MonoBehaviour
     private List<string> anim_list = new List<string>();
     // Start is called before the first frame update
 
+    //2-1 Gameobject
+    [Header("===== Gameobject =====")]
     private Animation Anim;
     public GameObject Object_1_blade1;
-    public GameObject Object_2_blade2;
-    public GameObject Object_3_blade3;
+    public GameObject Object_1_blade2;
+    public GameObject Object_1_blade3;
     public GameObject Object_4_Nacelle;
     public GameObject Object_5_Tower;
     public GameObject Object_6_Rotor;
     public GameObject Object_7_Shaft;
     public GameObject Object_8_Hub;
-    public GameObject Object_9_Pitch;
+    public GameObject Object_9_Pitch_bearing;
+    public GameObject Object_9_1_Pitch_sytem_1;
+    public GameObject Object_9_1_Pitch_sytem_2;
+    public GameObject Object_9_1_Pitch_sytem_3;
     public GameObject Object_10_Spinner;
     public GameObject Object_11_Mainshaft;
     public GameObject Object_12_Yaw;
     public GameObject Object_13_Gearbox;
     public GameObject Object_14_Generator;
 
+
+    private int BtnCount;
+
     void Start()
     {
         Anim = Main_object.GetComponent<Animation>();
         
         Camera.GetComponent<Camera_movement>().enabled = false;
-        Invoke("StartAct", 3f);
-        Object_Col_Off();
+        StartCoroutine(StartAct());
+        Object_Col_Off_ALL();
     }
-    void StartAct()
+    IEnumerator StartAct()
     {
-        //하단 스크립트, 인트로 2 애니메이션
-        Study_title.SetActive(true);
+        yield return new WaitForSeconds(3.0f);
+        Study_title_Intro_2.SetActive(true);
+        Study_title_Intro_2.GetComponent<Animation>().Play("Intro_2_animation(on)");
         Scriptbox.GetComponent<Animation>().Play("bannerup(1220)");
         Top_navigation.GetComponent<Animation>().Play("TN_intro_down");
+        yield break;
     }
-   
+
     void Act1()
     {
         if (Prev_Status == true)
@@ -62,22 +73,28 @@ public class Scene_1_2_controller : MonoBehaviour
             //카메라, 발전기 위치 재설정
 
         }
-        Object_Col_Off();
-        Object_1_blade1.GetComponent<MeshCollider>().enabled = true;
-        Object_2_blade2.GetComponent<MeshCollider>().enabled = true;
-        Object_3_blade3.GetComponent<MeshCollider>().enabled = true;
-        Object_4_Nacelle.GetComponent<MeshCollider>().enabled = true;
-        Object_5_Tower.GetComponent<MeshCollider>().enabled = true;
-        Object_6_Rotor.GetComponent<MeshCollider>().enabled = true;
+
+        //콜라이더(툴팁, 하이라이트)
+        Object_Col_Off_ALL();
+
+        Object_Col_On(Object_1_blade1);
+        Object_Col_On(Object_1_blade2);
+        Object_Col_On(Object_1_blade3);
+        Object_Col_On(Object_4_Nacelle);
+        Object_Col_On(Object_5_Tower);
+        Object_Col_On(Object_6_Rotor);
+
+
+        //애니메이션
         Camera.GetComponent<Camera_movement>().enabled = true;
-        Study_title.GetComponent<Animation>().Play("Intro_2_animation(off)");
-        Anim.Play("1_WTG_rotation");
+        Study_title_Intro_2.GetComponent<Animation>().Play("Intro_2_animation(off)");
+        //Anim.Play("1_WTG_rotation");
+        StartCoroutine(Animation_play(1));
         Wind_particle.SetActive(true);
-        //인트로 2
         Debug.Log("act1");
     }
     //블레이드
-    void Act2()
+    void Act3()
     {
         if (Prev_Status == true)
         {
@@ -85,62 +102,181 @@ public class Scene_1_2_controller : MonoBehaviour
             //Camera.GetComponent<Camera_movement>().act2();
         }
 
-        //this.GetComponent<NarrationController>().Audio.clip = this.GetComponent<NarrationController>().AudioFiles[1];
+        //콜라이더(툴팁, 하이라이트)
+        Object_Col_Off_ALL();
+        Object_Col_On(Object_1_blade1);
+        Object_Col_On(Object_1_blade2);
+        Object_Col_On(Object_1_blade3);
 
-        //Camera.GetComponent<Camera_movement>().act1();
 
-        //바람 효과 제거 하고 풍력발전기 정지, 그리고 블레이드만 앞으로 살짝 O
-        Object_Col_Off();
-        Object_1_blade1.GetComponent<MeshCollider>().enabled = true;
-        Object_2_blade2.GetComponent<MeshCollider>().enabled = true;
-        Object_3_blade3.GetComponent<MeshCollider>().enabled = true;
+        //하이라이트 효과
+        StartCoroutine(Highlight_onoff(Object_1_blade1));
+        StartCoroutine(Highlight_onoff(Object_1_blade2));
+        StartCoroutine(Highlight_onoff(Object_1_blade3));
+
+
+        //애니메이션
         Wind_particle.SetActive(false);
         Camera.GetComponent<Camera_movement>().act1();
         Anim.Stop();
-        Anim.Play("3_WTG_blade_move");
-        Debug.Log("act2");
-    }
-    void Act3()
-    {
-        //블레이드, 바람 불고 다시 원위치, 회전
-        //Anim.Play("WTG_reset_rotation(3)");
-        Wind_particle.SetActive(true);
-        Object_6_Rotor.GetComponent<MeshCollider>().enabled = true;
-        Camera.GetComponent<Camera_movement>().act2();
-        Anim.Play("3_WTG_blade_move(return)");
+        //Anim.Play("2_WTG_blade_move");
+        StartCoroutine(Animation_play(2));
         Debug.Log("act3");
-
     }
     void Act4()
     {
-        Object_Col_Off();
-        Object_6_Rotor.GetComponent<MeshCollider>().enabled = true;
-        Object_8_Hub.GetComponent<MeshCollider>().enabled = true;
-        Object_9_Pitch.GetComponent<MeshCollider>().enabled = true;
-        Object_10_Spinner.GetComponent<MeshCollider>().enabled = true;
-        Camera.GetComponent<Camera_movement>().act3();
-        Anim.Play("4_WTG_rotor_move");
-        
+        if (Prev_Status == true)
+        {
+            //
+
+            Prev_Status = false;
+        }
+
+
+        //하이라이트 효과
+        StartCoroutine(Highlight_onoff(Object_1_blade1));
+        StartCoroutine(Highlight_onoff(Object_1_blade2));
+        StartCoroutine(Highlight_onoff(Object_1_blade3));
+
+        //애니메이션
+        //Anim.Play("3_2_WTG_blade_pitch");
+        StartCoroutine(Animation_play(3.2));
         Debug.Log("act4");
     }
     void Act5()
     {
-        Object_Col_Off();
-        Object_8_Hub.GetComponent<MeshCollider>().enabled = true;
-        Camera.GetComponent<Camera_movement>().act4();
+        //콜라이더(툴팁, 하이라이트)
+        Object_Col_Off_ALL();
+        Object_Col_On(Object_6_Rotor);
+
+        //하이라이트 효과
+        StartCoroutine(Highlight_onoff(Object_6_Rotor));
+
+        //애니메이션
+        Wind_particle.SetActive(true);
+        Camera.GetComponent<Camera_movement>().act2();
+        //Anim.Stop();
+        StartCoroutine(Animation_play(2.1));    //재조립 및 회전
         Debug.Log("act5");
+
     }
     void Act6()
     {
-        Object_Col_Off();
-        Object_9_Pitch.GetComponent<MeshCollider>().enabled = true;
-        Anim.Play("5_WTG_blade_pitch");
+        //주축 내용 추가?
+        //콜라이더(툴팁, 하이라이트)
+        Object_Col_Off_ALL();
+        Object_Col_On(Object_6_Rotor);
+
+        //하이라이트 효과
+        StartCoroutine(Highlight_onoff(Object_6_Rotor));
+
+
+        //애니메이션
+        //Anim.Play("5_WTG_rotor_move");
+        StartCoroutine(Animation_play(5));
+
         Debug.Log("act6");
     }
     void Act7()
     {
+        //콜라이더(툴팁, 하이라이트)
+        Object_Col_Off_ALL(); 
+        Object_Col_On(Object_6_Rotor);
+        Object_Col_On(Object_8_Hub);
+        Object_Col_On(Object_9_Pitch_bearing);
+        Object_Col_On(Object_10_Spinner);
+        Object_Col_On(Object_9_1_Pitch_sytem_1);
+        Object_Col_On(Object_9_1_Pitch_sytem_2);
+        Object_Col_On(Object_9_1_Pitch_sytem_3);
+
+        //하이라이트 효과
+        StartCoroutine(Highlight_onoff(Object_6_Rotor));
+        StartCoroutine(Highlight_onoff(Object_8_Hub));
+        StartCoroutine(Highlight_onoff(Object_9_Pitch_bearing));
+        StartCoroutine(Highlight_onoff(Object_10_Spinner));
+        StartCoroutine(Highlight_onoff(Object_9_1_Pitch_sytem_1));
+
+        //애니메이션
+        Camera.GetComponent<Camera_movement>().act3();
+        Anim.Play("5_1_WTG_rotor_move(hub,spinner)");
+        StartCoroutine(Animation_play(5.1));
+        //StartCoroutine(Animation_play(1));
+        //Anim.Play("1_WTG_rotation");
+    }
+    void Act8()
+    {
+        //콜라이더(툴팁, 하이라이트)
+        Object_Col_Off_ALL();
+        Object_Col_On(Object_8_Hub);
+        Object_Col_On(Object_11_Mainshaft);
+
+        //하이라이트 효과
+        StartCoroutine(Highlight_onoff(Object_8_Hub));
+        StartCoroutine(Highlight_onoff(Object_11_Mainshaft));
+
+        //애니메이션
+        Camera.GetComponent<Camera_movement>().act4();
+        //5.2로 변경
+        StartCoroutine(Animation_play(5.2));
+        Debug.Log("act5");
+    }
+    void Act9()
+    {
+        Object_Col_Off_ALL();
+        //블레이드 분리, 베어링 회전
+        Object_Col_On(Object_9_1_Pitch_sytem_1);
+        Object_Col_On(Object_9_Pitch_bearing);
+
+        StartCoroutine(Highlight_onoff(Object_9_1_Pitch_sytem_1));
+        StartCoroutine(Highlight_onoff(Object_9_Pitch_bearing));
+
+        Camera.GetComponent<Camera_movement>().act4();
+        //5.2로 변경
+        StartCoroutine(Animation_play(6));
+        Debug.Log("act5");
+    }
+    void Act10()
+    {
+        Object_Col_Off_ALL();
+        //블레이드 결합, 세개다 제어 시작(기존꺼 가져다 쓰기)
+        //하이라이트 없음
+        Object_Col_On(Object_8_Hub);
+        Object_Col_On(Object_11_Mainshaft);
+
+        StartCoroutine(Highlight_onoff(Object_8_Hub));
+        StartCoroutine(Highlight_onoff(Object_11_Mainshaft));
+
+        Camera.GetComponent<Camera_movement>().act4();
+        //5.2로 변경
+        StartCoroutine(Animation_play(6));
+        Debug.Log("act5");
+    }
+    void Act11()
+    {
+        Object_Col_Off_ALL();
+        //나셀 클로즈업 및 분리
+        Object_Col_On(Object_8_Hub);
+        Object_Col_On(Object_11_Mainshaft);
+
+        StartCoroutine(Highlight_onoff(Object_8_Hub));
+        StartCoroutine(Highlight_onoff(Object_11_Mainshaft));
+
+        Camera.GetComponent<Camera_movement>().act4();
+        //5.2로 변경
+        StartCoroutine(Animation_play(6));
+        Debug.Log("act5");
+    }
+    void Act77()
+    {
+        Object_Col_Off_ALL();
+        Object_9_Pitch_bearing.GetComponent<MeshCollider>().enabled = true;
+        Anim.Play("5_WTG_blade_pitch");
+        Debug.Log("act6");
+    }
+    void Act88()
+    {
         //나셀 내부부품 툴팁 추가하기
-        Object_Col_Off();
+        Object_Col_Off_ALL();
         Object_11_Mainshaft.GetComponent<MeshCollider>().enabled = true;
         Object_12_Yaw.GetComponent<MeshCollider>().enabled = true;
         Object_13_Gearbox.GetComponent<MeshCollider>().enabled = true;
@@ -149,79 +285,100 @@ public class Scene_1_2_controller : MonoBehaviour
         Anim.Play("6_WTG_blade_pitch(return)+nacelle_open");
         Debug.Log("act7");
     }
-    void Act8()
+    void Act19()
     {
         //나셀 내부부품 툴팁 추가하기
-        Object_Col_Off();
+        Object_Col_Off_ALL();
         Object_12_Yaw.GetComponent<MeshCollider>().enabled = true;
         Camera.GetComponent<Camera_movement>().act6();
         Anim.Play("7_WTG_Nacelle_rotation");
         Debug.Log("act8");
     }
-
-    //애니메이션 재생하면서 오브젝트 물리엔진 일시정지 및 활성화
-    //void KineticDisable()
-    //{
-    //    Transform AllMetalball = Metalball.GetComponentInChildren<Transform>();
-    //    foreach (Transform child in AllMetalball)
-    //    {
-    //        child.GetComponent<Rigidbody>().isKinematic = true;
-    //    }
-    //}
-    //void KineticEnable()
-    //{
-    //    Transform AllMetalball = Metalball.GetComponentInChildren<Transform>();
-    //    foreach (Transform child in AllMetalball)
-    //    {
-    //        child.GetComponent<Rigidbody>().isKinematic = false;
-    //    }
-    //}
-
     // Update is called once per frame
     void Update()
     {
         BtnCountToggle();
-        if (gameObject.GetComponent<Script_controller>().btnCount == 0 && toggle)
+
+        if (nowCount == 0 && toggle)
         {
             
             toggle = false;
         }
-        if (gameObject.GetComponent<Script_controller>().btnCount == 1 && toggle)
+        if (nowCount == 1 && toggle)
         {
             Act1();
             toggle = false;
         }
-        if (gameObject.GetComponent<Script_controller>().btnCount == 2 && toggle)
+        if (nowCount == 2 && toggle)
         {
-            Act2();
             toggle = false;
         }
-        if (gameObject.GetComponent<Script_controller>().btnCount == 3 && toggle)
+        if (nowCount == 3 && toggle)
         {
             Act3();
             toggle = false;
         }
-        if (gameObject.GetComponent<Script_controller>().btnCount == 4 && toggle)
+        if (nowCount == 4 && toggle)
         {
             Act4();
             toggle = false;
         }
-        if (gameObject.GetComponent<Script_controller>().btnCount == 5 && toggle)
+        if (nowCount == 5 && toggle)
         {
             Act5();
             toggle = false;
         }
-        if (gameObject.GetComponent<Script_controller>().btnCount == 6 && toggle)
+        if (nowCount == 6 && toggle)
         {
             Act6();
             toggle = false;
         }
-        if (gameObject.GetComponent<Script_controller>().btnCount == 7 && toggle)
+        if (nowCount == 7 && toggle)
         {
             Act7();
             toggle = false;
         }
-        if (gameObject.GetComponent<Script_controller>().btnCount == 8 && toggle)
+        if (nowCount == 8 && toggle)
+        {
+            Act8();
+            toggle = false;
+        }
+        if (nowCount == 9 && toggle)
+        {
+            Act9();
+            toggle = false;
+        }
+        if (nowCount == 10 && toggle)
+        {
+            Act8();
+            toggle = false;
+        }
+        if (nowCount == 11 && toggle)
+        {
+            Act8();
+            toggle = false;
+        }
+        if (nowCount == 12 && toggle)
+        {
+            Act8();
+            toggle = false;
+        }
+        if (nowCount == 13 && toggle)
+        {
+            Act8();
+            toggle = false;
+        }
+        if (nowCount == 14 && toggle)
+        {
+            Act8();
+            toggle = false;
+        }
+        if (nowCount == 15 && toggle)
+        {
+            Act8();
+            toggle = false;
+        }
+        if (nowCount == 16 && toggle)
         {
             Act8();
             toggle = false;
@@ -243,26 +400,155 @@ public class Scene_1_2_controller : MonoBehaviour
         }
         postCount = nowCount;
     }
-    private void Object_Col_Off()
+    private void Object_Col_Off_ALL()
     {
         Object_1_blade1.GetComponent<MeshCollider>().enabled = false;
-        Object_2_blade2.GetComponent<MeshCollider>().enabled = false;
-        Object_3_blade3.GetComponent<MeshCollider>().enabled = false;
+        Object_1_blade2.GetComponent<MeshCollider>().enabled = false;
+        Object_1_blade3.GetComponent<MeshCollider>().enabled = false;
         Object_4_Nacelle.GetComponent<MeshCollider>().enabled = false;
         Object_5_Tower.GetComponent<MeshCollider>().enabled = false;
         Object_6_Rotor.GetComponent<MeshCollider>().enabled = false;
         Object_7_Shaft.GetComponent<MeshCollider>().enabled = false;
         Object_8_Hub.GetComponent<MeshCollider>().enabled = false;
-        Object_9_Pitch.GetComponent<MeshCollider>().enabled = false;
+        Object_9_Pitch_bearing.GetComponent<MeshCollider>().enabled = false;
         Object_10_Spinner.GetComponent<MeshCollider>().enabled = false;
         Object_11_Mainshaft.GetComponent<MeshCollider>().enabled = false;
         Object_12_Yaw.GetComponent<MeshCollider>().enabled = false;
         Object_13_Gearbox.GetComponent<MeshCollider>().enabled = false;
         Object_14_Generator.GetComponent<MeshCollider>().enabled = false;
+
+        Object_9_1_Pitch_sytem_1.GetComponent<MeshCollider>().enabled = false;
+        Object_9_1_Pitch_sytem_2.GetComponent<MeshCollider>().enabled = false;
+        Object_9_1_Pitch_sytem_3.GetComponent<MeshCollider>().enabled = false;
     }
 
-    private void Anim_act1()
+    private void Object_Col_On(GameObject obj)
     {
-
+        obj.GetComponent<MeshCollider>().enabled = true;
     }
+
+    IEnumerator Animation_play(double num)
+    {
+        if (num == 1)
+        {
+            //풍력발전기 회전
+            Anim.Play("1_WTG_rotation");
+        }
+        else if (num == 2)
+        {
+            //각 블레이드 이동
+            Anim.Play("2_WTG_blade_move");
+        }
+        else if (num == 2.1)
+        {
+            //각 블레이드 원위치 이동
+            Anim.Play("2_1_WTG_blade_move(return)");
+        }
+        else if (num == 3)
+        {
+            Anim.Play("3_WTG_blade_pitch(retrurn)");
+        }
+        else if (num == 3.2)
+        {
+            //각 블레이드 피치 회전
+            Anim.Play("3_2_WTG_blade_pitch(rotation)");
+        }
+        else if (num == 5)
+        {
+            //로터만 이동
+            Anim.Play("5_WTG_rotor_move");
+        }
+        else if (num == 5.1)
+        {
+            //로터 허브 스피터 분리 및 이동
+            Anim.Play("5_1_WTG_rotor_move(hub,spinner)");
+        }
+        else if (num == 5.2)
+        {
+            //로터, 스피너,허브 재조립 및 나셀 분리
+            Anim.Play("5_2_WTG_rotor_move_(return)+nacelle_open");
+        }
+        else if (num == 6)
+        {
+            //나셀 분리
+            Anim.Play("6_WTG_Nacelle_open");
+        }
+        else if (num == 6.1)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        else if (num == 10)
+        {
+            Anim.Play("6_1_WTG_Nacelle_rotation");
+        }
+        yield break;
+    }
+
+    IEnumerator Highlight_onoff(GameObject obj)
+    {
+        obj.GetComponent<HighlightEffect>().highlighted = true;
+        yield return new WaitForSeconds(3.0f);
+        obj.GetComponent<HighlightEffect>().highlighted = false;
+        yield break;
+    }
+
+    //애니메이션 재생하면서 오브젝트 물리엔진 일시정지 및 활성화
+    //void KineticDisable()
+    //{
+    //    Transform AllMetalball = Metalball.GetComponentInChildren<Transform>();
+    //    foreach (Transform child in AllMetalball)
+    //    {
+    //        child.GetComponent<Rigidbody>().isKinematic = true;
+    //    }
+    //}
+    //void KineticEnable()
+    //{
+    //    Transform AllMetalball = Metalball.GetComponentInChildren<Transform>();
+    //    foreach (Transform child in AllMetalball)
+    //    {
+    //        child.GetComponent<Rigidbody>().isKinematic = false;
+    //    }
+    //}
+
 }
