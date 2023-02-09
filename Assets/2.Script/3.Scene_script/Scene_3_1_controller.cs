4d17965c0dc6b7dfb6697502dc63f75d0308b1c6
Jeongseongwon <j001656100@gmail.com>
2023-02-09 19:12:15 +0900

    0209
    * 평가하기, 3_1 추가 작업 진행 프리팹 분할

--- Assets/2.Script/3.Scene_script/Scene_3_1_controller.cs
+++ Assets/2.Script/3.Scene_script/Scene_3_1_controller.cs
@@ -4,36 +4,35 @@ using UnityEngine;
 
 public class Scene_3_1_controller : MonoBehaviour
 {
+    public GameObject PC_Image;
     public GameObject[] PC_Image_Array;
     public GameObject Scriptbox;
     public GameObject Top_navigation;
-    public GameObject Intro_2;
+    public GameObject Study_title;
     int PostCount;
-
-    private bool flag = false;
-    private bool Prev_Status = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        PostCount = -1;
+
         //PC_Image_Array = PC_Image.gameObject.GetComponentsInChildren<Transform>();
         // PC_Image_Array = GameObject.FindGameObjectsWithTag("PC_Sprite");
-        
+        PC_Image.SetActive(false);
 
-        //Invoke("Startact", 2f);    // 5초 뒤에 해당 오브젝트 화면에 투사
-        //Invoke("PC_ON", 10f);    // 5초 뒤에 해당 오브젝트 화면에 투사
+        Invoke("Startact", 2f);    // 5초 뒤에 해당 오브젝트 화면에 투사
+        Invoke("PC_ON", 10f);    // 5초 뒤에 해당 오브젝트 화면에 투사
     }
     private void Startact() //중간 평가용으로 수정
     {
-        Intro_2.SetActive(true);
+        Study_title.SetActive(true);
         Scriptbox.GetComponent<Animation>().Play("bannerup(1220)");
         Top_navigation.GetComponent<Animation>().Play("TN_intro_down");
     }
     private void PC_ON()
     {
-        Intro_2.GetComponent<Animation>().Play("Intro_2_animation(off)");
+        Study_title.GetComponent<Animation>().Play("Intro_2_animation(off)");
 
+        PC_Image.SetActive(true);
         for (int i = 0; i < PC_Image_Array.Length; i++)
         {
             PC_Image_Array[i].gameObject.SetActive(false);
@@ -46,43 +45,21 @@ public class Scene_3_1_controller : MonoBehaviour
     {
         int BtnCount = gameObject.GetComponent<Script_controller>().btnCount;
         if (PostCount != BtnCount)
+            for (int i = 0; i < PC_Image_Array.Length; i++)
+            {
+                PC_Image_Array[i].gameObject.SetActive(false);
+                
+            }
+        //PC_Image_Array[0].gameObject.SetActive(true);
+        if (BtnCount == 1)
         {
-            if (BtnCount < PostCount)
-            {
-                Prev_Status = true;
-            }
-            //PC_Image_Array[PostCount].gameObject.SetActive(false);
-            flag = true;
+           // Startact();
         }
-
-        if (flag == true)
+        else if (BtnCount == 2)
         {
-
-            if (BtnCount == 0)
-            {
-                Startact();
-            }
-            else if (BtnCount == 1)
-            {
-                Intro_2.GetComponent<Animation>().Play("Intro_2_animation(off)");
-                Debug.Log("check_2");
-
-            }
-            if (Prev_Status==true)
-            {
-                //이전 버튼 클릭시
-                PC_Image_Array[BtnCount + 1].SetActive(false);
-                Prev_Status = false;
-            }
-
-            if (BtnCount > 0)
-            {
-                PC_Image_Array[BtnCount - 1].SetActive(false);
-            }
-            PC_Image_Array[BtnCount].SetActive(true);
-            PostCount = BtnCount;
-            flag = false;
-            Debug.Log("Image check");
+            Debug.Log("check_2");
         }
+        PC_Image_Array[BtnCount].SetActive(true);
+        PostCount = BtnCount;
     }
 }
