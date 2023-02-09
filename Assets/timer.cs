d3d2a9fe8c5cc094683decabe2de6ae0949c0f10
leehj67 <109390172+leehj67@users.noreply.github.com>
2023-02-09 19:01:33 +0900

    0229

---
+++ Assets/timer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timercount;
+    public GameObject nextpage;
+    private float clock=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clock += Time.deltaTime;
+        if(clock>=timercount)
+        {
+            gameObject.SetActive(false);
+            nextpage.SetActive(true);
+        }
+        
+    }
+}
